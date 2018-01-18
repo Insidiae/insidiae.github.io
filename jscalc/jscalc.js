7f1b435c4edd0b0f08806cec408dd7dfa312996e
Kobe Ruado <insidiae423@gmail.com>
2018-01-18 15:50:04 +0800

    Finished the JS Calculator project

--- jscalc/jscalc.js
+++ jscalc/jscalc.js
@@ -1,31 +1,34 @@
 var num1 = 0, num2 = 0, ans = 0;
-var neg1 = false, neg2 = false, hasDecimal = false;
-var onOperand2 = false, resetDisp = false;
+var hasDecimal = false, onOperand2 = false, resetDisp = false;
 var optn = "";
 
 
 $("button").click(function() {
     var pressed = $(this).attr("value");
     var dispText = $("#display").text();
-    console.log(typeof pressed);
     
     if($(this).hasClass("optr")) {
-        if(onOperand2) {
-            eval();
-            num1 = ans;
-            optn = $(this).attr("value");
-            nextOperand();
+        if(onOperand2 && num2 != 0) {
+            if(pressed == "neg") {
+                num2 = -num2;
+                $("#display").text(num2);
+                
+            } else {
+                eval();
+                num1 = ans;
+                optn = $(this).attr("value");
+                nextOperand();
+            }
         } else if(num1 !== 0 || ans !== 0) {
             if(ans !== 0) {
                 num1 = ans;
             }
             
             if(pressed == "neg") {
-                neg1 = !neg1;
-                console.log("neg1")
+                num1 = -num1;
+                $("#display").text(num1);
                 
             } else {
-                console.log("Insert Operation Logic Here");
                 optn = $(this).attr("value");
                 nextOperand();
             }
@@ -36,20 +39,26 @@ $("button").click(function() {
         
     } else if($(this).hasClass("clear")) {
         $("#display").text("0");
-        if(pressed == "ce") {
+        hasDecimal = false;
+        if(pressed == "ce" && ans == 0) {
             if(num1 !== 0) {
                 num2 = 0;
             } else {
                 num1 = 0;
             }
         } else {
+            onOperand2 = false;
             num2 = 0;
             num1 = 0;
             ans = 0;
         }
         
     } else if ($(this).hasClass("decimal")) {
-        
+        if(!hasDecimal && !resetDisp) {
+            dispText += pressed;
+            $("#display").text(dispText);
+            hasDecimal = true;
+        }
         
     } else if ($(this).hasClass("eval")) {
         eval();
@@ -59,16 +68,19 @@ $("button").click(function() {
             dispText = "";
             $("#display").text("");
         }
-        dispText += pressed;
-        $("#display").text(dispText);
         
-        if(onOperand2) {
-            num2 = Number(dispText);
-        } else {
-            num1 = Number(dispText);
+        if(dispText.length < 10) {
+            dispText += pressed;
+            $("#display").text(dispText);
+
+            if(onOperand2) {
+                num2 = Number(dispText);
+            } else {
+                num1 = Number(dispText);
+            }
+
+            resetDisp = false;
         }
-        
-        resetDisp = false;
     }
 })
 
@@ -82,18 +94,24 @@ function eval() {
     switch(optn) {
         case '+':
             ans = num1 + num2;
+            break;
         case '-':
             ans = num1 - num2;
+            break;
         case '*':
             ans = num1 * num2;
+            break;
         case "/":
             ans = num1 / num2;
+            break;
         default:
             ans = num1 + num2;
+            break;
     }
     num1 = 0;
     num2 = 0;
     $("#display").text(ans);
     onOperand2 = false;
-    resetDisp = false;
+    hasDecimal = false;
+    resetDisp = true;
 }

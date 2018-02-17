d5704f639f2cacf819df6e469b35bfb463ed40eb
Insidiae <insidiae423@gmail.com>
2018-02-18 01:45:02 +0800

    Finished #GRIDDYUP

---
+++ griddyup/responsive/responsive.js
@@ -0,0 +1,7 @@
+const navButton = document.querySelector("button[aria-expanded]");
+navButton.addEventListener("click", toggleNav);
+
+function toggleNav({ target }) {
+    const expanded = target.getAttribute("aria-expanded") === 'true' || false;
+    navButton.setAttribute("aria-expanded", !expanded);
+}
